5c1ce3da0bfc0fc90991edd2c416394a1be8896b
kuresarf <junk@fraseruk.net>
2016-11-13 21:24:09 +0000

    Updated page routing for new named html files.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /**
- * AngularJS Tutorial 1
- * @author Nick Kaye <nick.c.kaye@gmail.com>
+ * Originally based on AngularJS Tutorial 1 by Nick Kaye
+ * 
  */
 
 /**
@@ -18,14 +18,11 @@ app.config(['$routeProvider', function ($routeProvider) {
     // Home
     .when("/", {templateUrl: "partials/home.html", controller: "PageCtrl"})
     // Pages
-    .when("/about", {templateUrl: "partials/about.html", controller: "PageCtrl"})
-    .when("/faq", {templateUrl: "partials/faq.html", controller: "PageCtrl"})
-    .when("/pricing", {templateUrl: "partials/pricing.html", controller: "PageCtrl"})
-    .when("/services", {templateUrl: "partials/services.html", controller: "PageCtrl"})
-    .when("/contact", {templateUrl: "partials/contact.html", controller: "PageCtrl"})
-    // Blog
-    .when("/blog", {templateUrl: "partials/blog.html", controller: "BlogCtrl"})
-    .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "BlogCtrl"})
+    .when("/backpanel", {templateUrl: "partials/backpanel.html", controller: "PageCtrl"})
+    .when("/frontpanel", {templateUrl: "partials/frontpanel.html", controller: "PageCtrl"})
+    // Other Panels
+    .when("/otherpanels", {templateUrl: "partials/otherpanels.html", controller: "OtherPanelCtrl"})
+    .when("/blog/post", {templateUrl: "partials/blog_item.html", controller: "OtherPanelCtrl"})
     // else 404
     .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
 }]);
@@ -33,8 +30,8 @@ app.config(['$routeProvider', function ($routeProvider) {
 /**
  * Controls the Blog
  */
-app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
-  console.log("Blog Controller reporting for duty.");
+app.controller('OtherPanelCtrl', function (/* $scope, $location, $http */) {
+  console.log("OtherPanelCtrl Controller reporting for duty.");
 });
 
 /**
